64405b5d8f1dd69f46ceac16d503e1779065287a
Fernando Machado <lfmachado90@gmail.com>
2020-08-10 20:30:22 -0300

    View env var on heroku

--- api/main.go
+++ api/main.go
@@ -10,6 +10,7 @@ func main() {
 	api := API{cfg: cfg}
 
 	log.Println("Initializing server at port " + cfg.Port)
+	log.Println("hush hush " + cfg.InstagramSecret)
 	if err := http.ListenAndServe(":"+cfg.Port, api.Routes()); err != nil {
 		log.Fatalf("Failed to initialize server: %s", err)
 	}
