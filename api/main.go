ae2cf4c9c0b2d9998b205746628ea72fa8846d15
Fernando Machado <lfmachado90@gmail.com>
2020-08-10 20:33:01 -0300

    Clean up logs

--- api/main.go
+++ api/main.go
@@ -10,7 +10,6 @@ func main() {
 	api := API{cfg: cfg}
 
 	log.Println("Initializing server at port " + cfg.Port)
-	log.Println("hush hush " + cfg.InstagramSecret)
 	if err := http.ListenAndServe(":"+cfg.Port, api.Routes()); err != nil {
 		log.Fatalf("Failed to initialize server: %s", err)
 	}
